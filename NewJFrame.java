8bb0e6a5e92cd884aafd912bcc0ce41701772c83
vthanig8 <vthanig8@students.d125.org>
2016-05-11 08:26:33 -0500

    fixed xD

--- NewJFrame.java
+++ NewJFrame.java
@@ -25,11 +25,23 @@ public class NewJFrame extends javax.swing.JFrame {
      */
     public void paint(Graphics g)
     {
-        // simple text displayed on applet
-        g.setColor(Color.green);
-        g.fillRect(335, 20, 18, 180);
-        g.fillRect(309,184,70,16);
+       
         
+         g.setColor(Color.blue);
+        g.fillRect(50,300,40,10);
+        g.fillRect(75,70,5,232);
+        g.fillRect(75,70,75,5);
+        g.fillRect(125,70,5,50);
+        
+        
+        
+        g.setColor(Color.red);
+        g.fillOval(100,100,50,50);
+        g.fillRect(125,125,3,100);
+        g.drawLine(125,175,160,150);
+        g.drawLine(125,175,90,150);
+        g.drawLine(126,223,90,250);
+        g.drawLine(126,223,160,250);
     }
     
     /**
