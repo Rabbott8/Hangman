b2d87272c64b52a3a499c603be5e79fb13a25d8a
Rabbott8 <rabbott8@students.d125.org>
2016-05-23 08:54:29 -0500

    fixed letter clicked causing more than one repaint error

--- NewJFrame.java
+++ NewJFrame.java
@@ -12,27 +12,28 @@ import javax.swing.*;
  * @author rabbott8
  */
 public class NewJFrame extends JFrame {
-   private String str1 = "";
-   private int tC = 0;
-   private int x =150;
-   private int y = 0;
-   private int z=150;
-   private int a = 200;
-   private int b = 360;
-   private static int num = 0;
-   private static SecretWord thiarr = null;
-   private static ArrayList<Integer> guy = new ArrayList<Integer>();
-   private String str2 = "doge";
-   private String name =System.getProperty("user.name");
-   private String ios9 = System.getProperty("os.name");
-   private int ctr  = 1001;
+    private String str1 = "";
+    private int tC = 0;
+    private int x =150;
+    private int y = 0;
+    private int z=150;
+    private int a = 200;
+    private int b = 360;
+    private ArrayList<String> checker = new ArrayList<String>();
+    private boolean check = true;
+    private static int num = 0;
+    private static SecretWord thiarr = null;
+    private static ArrayList<Integer> guy = new ArrayList<Integer>();
+    private String str2 = "doge";
+    private String name =System.getProperty("user.name");
+    private String ios9 = System.getProperty("os.name");
+    private int ctr  = 1001;
     /**
      * Creates new form NewJFrame
      */
     public NewJFrame() {
         initComponents();
-        
-        
+
        
     }
 
@@ -43,73 +44,72 @@ public class NewJFrame extends JFrame {
      */
     public void paint(Graphics g)
     {
-         g.setFont(new Font("TimesNewRoman", Font.BOLD, 10));
-          g.drawString(name +"-" + ios9, 250, 50);
-          g.setFont(new Font("Impact", Font.BOLD, 20));
-          g.drawString("Hover Over The Letters To See Which Has Been Clicked" ,250,100);
-        if(guy==null )
-       {tC++;}
-       
+        g.setFont(new Font("TimesNewRoman", Font.BOLD, 10));
+        g.drawString(name +"-" + ios9, 250, 50);
+        g.setFont(new Font("Impact", Font.BOLD, 20));
+        g.drawString("Hover Over The Letters To See Which Has Been Clicked" ,250,100);
+        if(guy==null && check)
+        {tC++;}
+
         if (tC==0){g.setColor(Color.blue);
-        g.fillRect(50,300,55,10);
-        g.fillRect(75,70,5,232);
-        g.fillRect(75,70,75,5);
-        g.fillRect(125,70,5,50);}
+            g.fillRect(50,300,55,10);
+            g.fillRect(75,70,5,232);
+            g.fillRect(75,70,75,5);
+            g.fillRect(125,70,5,50);}
 
         g.setColor(Color.red);
-        
+
         //Head
-        if(tC == 1)
+        if(tC == 1 && check)
         {g.fillOval(100,100,50,50);
         }
 
         //Body
-        if(tC == 2){
-        g.fillRect(125,125,3,100);}
+        if(tC == 2 && check){
+            g.fillRect(125,125,3,100);}
 
         //arms
-        if(tC == 3){
-        g.drawLine(125,175,160,150);}
-        if(tC == 4){
-        g.drawLine(125,175,90,150);}
+        if(tC == 3 && check){
+            g.drawLine(125,175,160,150);}
+        if(tC == 4 && check){
+            g.drawLine(125,175,90,150);}
 
         //Legs
-        if(tC == 5){
-        g.drawLine(126,223,90,250);}
-        if(tC == 6){
-        g.drawLine(126,223,160,250);}
-        
-        if(tC==7){
-        //Eyes
-        g.setColor(Color.black);
-        g.drawLine(110,115,120,125);
-        g.drawLine(120,115,110,125);
-        g.drawLine(130,115,140,125);
-        g.drawLine(140,115,130,125);
+        if(tC == 5 && check){
+            g.drawLine(126,223,90,250);}
+        if(tC == 6 && check){
+            g.drawLine(126,223,160,250);}
 
-        //Mouth
-        g.drawArc(110,135,30,12,0,180);
-        
-    }
-        
+        if(tC==7 && check){
+            //Eyes
+            g.setColor(Color.black);
+            g.drawLine(110,115,120,125);
+            g.drawLine(120,115,110,125);
+            g.drawLine(130,115,140,125);
+            g.drawLine(140,115,130,125);
+
+            //Mouth
+            g.drawArc(110,135,30,12,0,180);
+
+        }
+
         //string
-       
-           g.setFont(new Font("TimesNewRoman", Font.BOLD, 30));
-            
-            
-         //x+=40;
-         //if (x>=700)
+
+        g.setFont(new Font("TimesNewRoman", Font.BOLD, 30));
+
+        //x+=40;
+        //if (x>=700)
         // {
-          //   x=190;
-          //   z+=50;
-         //   }
+        //   x=190;
+        //   z+=50;
+        //   }
         int c = num;
         //num = 5;
         while(num>0){
             g.setColor(Color.BLACK);
             g.drawLine(a,b,a+50,b);
             int n = 0;
-            
+
             if (guy!= null)
             {
                 for (int y = 0;y<guy.size();y++){
@@ -117,26 +117,26 @@ public class NewJFrame extends JFrame {
                     if(e>6){
                         e-=7;
                     }
-                g.drawString(str1,220+(70*(e)),360+((guy.get(y)/7)*50));}
+                    g.drawString(str1,220+(70*(e)),360+((guy.get(y)/7)*50));}
             }
             a+=70;
-        
-        if (a>=690)
+
+            if (a>=690)
             {
                 a = 200;
                 b = 410;
-                
+
             }
-        num--;
-        n++;
-        
+            num--;
+            n++;
+
+        }
+        a = 200;
+        b = 360;
+        num = c;
+        check = true;
+        y = 0;
     }
-       a = 200;
-       b = 360;
-       num = c;
-       
-       y = 0;
-}
 
     /**
     * This method is called from within the constructor to initialize the form.
@@ -146,9 +146,9 @@ public class NewJFrame extends JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
-        
+
         guy.add(0);
-        
+
         jButton1 = new JButton();
         jPanel1 = new JPanel();
         jButton3 = new JButton();
@@ -179,7 +179,6 @@ public class NewJFrame extends JFrame {
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        
 
         GroupLayout jPanel1Layout = new GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -208,7 +207,7 @@ public class NewJFrame extends JFrame {
 
         jButton4.setText("C");
         jButton4.addActionListener(new ActionListener()
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton4.setToolTipText("You have Used this Letter");
@@ -217,7 +216,7 @@ public class NewJFrame extends JFrame {
             });
         jButton5.setText("G");
         jButton5.addActionListener(new ActionListener()
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton5.setToolTipText("You have Used this Letter");
@@ -226,7 +225,7 @@ public class NewJFrame extends JFrame {
             });
         jButton6.setText("F");
         jButton6.addActionListener(new ActionListener()
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton6.setToolTipText("You have Used this Letter");
@@ -235,7 +234,7 @@ public class NewJFrame extends JFrame {
             });
         jButton7.setText("E");
         jButton7.addActionListener(new ActionListener()
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton7.setToolTipText("You have Used this Letter");
@@ -244,27 +243,27 @@ public class NewJFrame extends JFrame {
             });
         jButton2.setText("D");
         jButton2.addActionListener(new ActionListener() 
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton2.setToolTipText("You have Used this Letter");
-                       buttonAction("d");
+                    buttonAction("d");
                 }
             });
 
         jButton8.setText("H");
         jButton8.addActionListener(new ActionListener()
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton8.setToolTipText("You have Used this Letter");
                     buttonAction("h");
-                  
+
                 }
             });
         jButton9.setText("I");
         jButton9.addActionListener(new ActionListener()
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton9.setToolTipText("You have Used this Letter");
@@ -273,7 +272,7 @@ public class NewJFrame extends JFrame {
             });
         jButton10.setText("J");
         jButton10.addActionListener(new ActionListener() 
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton10.setToolTipText("You have Used this Letter");
@@ -283,7 +282,7 @@ public class NewJFrame extends JFrame {
 
         jButton11.setText("K");
         jButton11.addActionListener(new ActionListener() 
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton11.setToolTipText("You have Used this Letter");
@@ -293,7 +292,7 @@ public class NewJFrame extends JFrame {
 
         jButton12.setText("L");
         jButton12.addActionListener(new ActionListener() 
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton12.setToolTipText("You have Used this Letter");
@@ -303,7 +302,7 @@ public class NewJFrame extends JFrame {
 
         jButton13.setText("M");
         jButton13.addActionListener(new ActionListener() 
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton13.setToolTipText("You have Used this Letter");
@@ -313,7 +312,7 @@ public class NewJFrame extends JFrame {
 
         jButton14.setText("N");
         jButton14.addActionListener(new ActionListener() 
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton14.setToolTipText("You have Used this Letter");
@@ -323,7 +322,7 @@ public class NewJFrame extends JFrame {
 
         jButton15.setText("W");
         jButton15.addActionListener(new ActionListener() 
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton15.setToolTipText("You have Used this Letter");
@@ -333,7 +332,7 @@ public class NewJFrame extends JFrame {
 
         jButton16.setText("R");
         jButton16.addActionListener(new ActionListener() 
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton16.setToolTipText("You have Used this Letter");
@@ -342,7 +341,7 @@ public class NewJFrame extends JFrame {
             });
         jButton18.setText("O");
         jButton18.addActionListener(new ActionListener() 
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton18.setToolTipText("You have Used this Letter");
@@ -351,7 +350,7 @@ public class NewJFrame extends JFrame {
             });
         jButton19.setText("P");
         jButton19.addActionListener(new ActionListener() 
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton19.setToolTipText("You have Used this Letter");
@@ -360,7 +359,7 @@ public class NewJFrame extends JFrame {
             });
         jButton20.setText("X");
         jButton20.addActionListener(new ActionListener() 
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton20.setToolTipText("You have Used this Letter");
@@ -369,7 +368,7 @@ public class NewJFrame extends JFrame {
             });
         jButton21.setText("Z");
         jButton21.addActionListener(new ActionListener() 
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton21.setToolTipText("You have Used this Letter");
@@ -378,7 +377,7 @@ public class NewJFrame extends JFrame {
             });
         jButton22.setText("Y");
         jButton22.addActionListener(new ActionListener() 
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton22.setToolTipText("You have Used this Letter");
@@ -387,7 +386,7 @@ public class NewJFrame extends JFrame {
             });
         jButton23.setText("Q");
         jButton23.addActionListener(new ActionListener() 
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton23.setToolTipText("You have Used this Letter");
@@ -396,7 +395,7 @@ public class NewJFrame extends JFrame {
             });
         jButton25.setText("V");
         jButton25.addActionListener(new ActionListener() 
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton25.setToolTipText("You have Used this Letter");
@@ -405,7 +404,7 @@ public class NewJFrame extends JFrame {
             });
         jButton26.setText("U");
         jButton26.addActionListener(new ActionListener() 
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton26.setToolTipText("You have Used this Letter");
@@ -414,7 +413,7 @@ public class NewJFrame extends JFrame {
             });
         jButton24.setText("S");
         jButton24.addActionListener(new ActionListener() 
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton24.setToolTipText("You have Used this Letter");
@@ -423,12 +422,12 @@ public class NewJFrame extends JFrame {
             });
         jButton17.setText("T");
         jButton17.addActionListener(new ActionListener() 
-        {
+            {
                 public void actionPerformed(ActionEvent evt) 
                 {
                     jButton17.setToolTipText("You have Used this Letter");
                     buttonAction("t");
-                    
+
                 }
             });
         GroupLayout layout = new GroupLayout(getContentPane());
@@ -542,33 +541,39 @@ public class NewJFrame extends JFrame {
 
         pack();
     }// </editor-fold>                        
-    
+
     public static void numSpaces(int i)
     {
         num = i;
     }
-    
+
     public static void setArray(SecretWord p)
     {
         thiarr = p;
     }
-    
+
     public String buttonAction(String str){
         str1 = str;
         guy = thiarr.isLetter(str);
-        
+        check = true;
+        if (tC == 0)
+            repaint();
+
+        for (int i = 0; i<checker.size();i++)
+        {
+            if (checker.get(i).equals(str1))
+                check = false;
+        }
         repaint();
-          
-        
+        checker.add(str);
+        //check = true;
+
         return str;
-        
     }
     public String getString(){
-       
         return str1;
     }
-       
-            
+
     // Variables declaration - do not modify                     
     private JButton jButton1;
     private JButton jButton10;
@@ -597,6 +602,6 @@ public class NewJFrame extends JFrame {
     private JButton jButton8;
     private JButton jButton9;
     private JPanel jPanel1;
-   
+
     // End of variables declaration                   
 }
