0d287b74e240945093bc603821d8896dfb1bdef8
Rabbott8 <rabbott8@students.d125.org>
2016-05-24 09:10:30 -0500

    simple formatting fix

--- fgh.java
+++ fgh.java
@@ -18,32 +18,31 @@ public class fgh
      */
     public static void main(String args[])
     {
-        
-        SecretWord w = new SecretWord();
-       
-       NewJFrame.numSpaces(w.numLetters());
-       NewJFrame.setArray(w);
-        
-                    NewJFrame dogescreaming = new NewJFrame();
-                    dogescreaming.setVisible(true);
-                    dogescreaming.setResizable(false);
-                    dogescreaming.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                   Sound doge = new Sound();
-                   doge.play();
-       
-       System.out.print(w.numLetters());
-       
-       System.out.print(w);
-       
-       w.isLetter("e");
-       w.isLetter("i");
-       w.isLetter("a");
-       w.isLetter("o");
 
-       System.out.print(w);
-       
-       
+        SecretWord w = new SecretWord();
+
+        NewJFrame.numSpaces(w.numLetters());
+        NewJFrame.setArray(w);
+
+        NewJFrame dogescreaming = new NewJFrame();
+        dogescreaming.setVisible(true);
+        dogescreaming.setResizable(false);
+        dogescreaming.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Sound doge = new Sound();
+        doge.play();
+
+        System.out.print(w.numLetters());
+
+        System.out.print(w);
+
+        w.isLetter("e");
+        w.isLetter("i");
+        w.isLetter("a");
+        w.isLetter("o");
+
+        System.out.print(w);
+
        
     }
-    }
+}
 
