3ee79881ded0bd331d9c823baef2ac99b1d120fd
Rabbott8 <rabbott8@students.d125.org>
2016-05-26 00:56:37 -0500

    added documentation

--- FileReaderClass.java
+++ FileReaderClass.java
@@ -1,17 +1,20 @@
 
+
+
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
- * Reads and prints text from a file
+ * Reads one line of text from a file
+ * Precondition: text is only one word
  * @author rabbott8
  * @author vthanig8
  * @author dtarson8
  * @author akunche8
 
  */
-import java.io.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class FileReaderClass
 {
     public static void main(String[] args) 
